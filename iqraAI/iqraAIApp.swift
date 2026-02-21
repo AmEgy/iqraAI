a64cf7dc875db62537c3e2b9581b9e7147c37e3a
AdamMagued <adamismailmageud@gmail.com>
2026-02-21 19:54:16 +0200

    Initial Commit

---
+++ iqraAI/iqraAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iqraAIApp.swift
+//  iqraAI
+//
+//  Created by Adam Maged on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct iqraAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
